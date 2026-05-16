f0b7650569dc9d43877b2ae5bb8c5da66e408877
Avraneel Pal <avraneelpal22@gmail.com>
2026-05-16 17:00:30 +0000

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,10 @@
-export default function App() {}
+import General from "./General";
+
+export default function App() {
+  return (
+    <>
+      <h1>CV Application</h1>
+      <General />
+    </>
+  );
+}
